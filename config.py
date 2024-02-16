ccf6bc493ab671883af44c26353e89e57218ca8d
Sai <90199989+djsaikrishna@users.noreply.github.com>
2024-02-16 23:51:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-API_ID =   # api id
-API_HASH = ""  # api hash
+API_ID = 21008813  # api id
+API_HASH = "c12311d0c53d2af00d159cb3156fc29e"  # api hash
 
-BOT_TOKEN = ""  # bot token
+BOT_TOKEN = "6778484389:AAFqOoTn_nZDhqK-wY-T6qKppy9RCg8OfhM"  # bot token
 
 
 ## REDIS
-HOST = ""  # redis host uri
+HOST = "redis-15392.c304.europe-west1-2.gce.cloud.redislabs.com:15392"  # redis host uri
 PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+PASSWORD = "cbw7xHuW3uJmddWJRRqxiSWmRvKi81GM"  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002064454124  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (dont use this)
-ADMINS = [1317173146]
+ADMINS = [6576587845]
